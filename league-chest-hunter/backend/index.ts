447f2f20fa8bd14b2bc1a695aa5b6bf4f78d97c4
Odd Harald Sjursen Sande <oddh123@gmail.com>
2021-11-21 13:53:25 +0100

    Add league-chest-hunter endpoint

--- league-chest-hunter/backend/index.ts
+++ league-chest-hunter/backend/index.ts
@@ -1,7 +1,55 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-function handler(req) {
-  return new Response("Hello world");
+const RIOT_API_ROOT_LOL = config().RIOT_API_ROOT_LOL;
+const API_KEY_TOKEN = config().RIOT_API_KEY;
+console.log(API_KEY_TOKEN);
+
+async function fetchSummonerId(
+  summonerName: string,
+  summonerId: string | undefined = undefined,
+) {
+  if (summonerId) {
+    return summonerId;
+  }
+  return await fetch(
+    `${RIOT_API_ROOT_LOL}summoner/v4/summoners/by-name/${summonerName}`,
+    {
+      headers: {
+        "X-Riot-Token": API_KEY_TOKEN,
+      },
+    },
+  )
+    .then((response) => (response.json()))
+    .then((res: { id: string }) => res.id);
+}
+
+function fetchChampionMastery(summonerId: string) {
+  return fetch(
+    `${RIOT_API_ROOT_LOL}champion-mastery/v4/champion-masteries/by-summoner/${summonerId}`,
+    {
+      headers: {
+        "X-Riot-Token": API_KEY_TOKEN,
+      },
+    },
+  );
+}
+
+async function handler(req: any) {
+  if (req.query.name || req.summonerId) {
+    const summonerId = await fetchSummonerId(
+      req.body.summonerId,
+      req.query.name,
+    );
+
+    const response = await fetchChampionMastery(summonerId).then((response) =>
+      response.json()
+    );
+    return new Response(`{ "championMastery": ${response}, ${summonerId} }`);
+  }
+  return new Response("Error: Include either a summerName or summonerId", {
+    status: 400,
+  });
 }
 
 console.log("Listening on http://localhost:8000");
