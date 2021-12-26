6e6a0d9b2a164adb761b504f1470ffa4890942f6
Oddpod <oddh123@gmail.com>
2021-12-26 18:04:42 +0100

    cleanup errors

--- league-chest-hunter/functions/getChampMastery.ts
+++ league-chest-hunter/functions/getChampMastery.ts
@@ -1,18 +1,32 @@
-
 import { Handler } from "@netlify/functions";
-const fetch = require('node-fetch')
+import fetch from "node-fetch";
 
 const { RIOT_API_ROOT_LOL, API_KEY_TOKEN } = process.env;
+
+type FetchChampionMasteryQuery = {
+  name: string | undefined;
+  id: string | undefined;
+};
+
+export interface SummonerResponse {
+  id: string;
+  accountID: string;
+  puuid: string;
+  name: string;
+  profileIconID: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
 
 //#region helper functions
 function fetchData(url: string) {
   return fetch(url, {
-    method: 'get',
+    method: "get",
     headers: {
-      'Content-Type': 'application/json',
-      'X-Riot-Token': API_KEY_TOKEN
-    }
-  }).then(res => res.json());
+      "Content-Type": "application/json",
+      "X-Riot-Token": API_KEY_TOKEN,
+    },
+  }).then((res) => res.json());
 }
 async function fetchSummonerId(
   summonerName: string,
@@ -21,35 +35,35 @@ async function fetchSummonerId(
   if (summonerId) {
     return summonerId;
   }
-  const res = await fetchData(
+  const res = (await fetchData(
     `${RIOT_API_ROOT_LOL}summoner/v4/summoners/by-name/${summonerName}`
-  )
+  )) as SummonerResponse;
   return res.id;
 }
 
 function fetchChampionMastery(summonerId: string) {
   return fetchData(
-    `${RIOT_API_ROOT_LOL}champion-mastery/v4/champion-masteries/by-summoner/${summonerId}`,
+    `${RIOT_API_ROOT_LOL}champion-mastery/v4/champion-masteries/by-summoner/${summonerId}`
   );
 }
 //#endregion
 
-
-
 const handler: Handler = async (event, context) => {
-  const { name: summonerName = undefined, id = undefined } = event.queryStringParameters;
+  const { name: summonerName = undefined, id = undefined } =
+    event.queryStringParameters as FetchChampionMasteryQuery;
   if (summonerName || id) {
-    const summonerId = await fetchSummonerId(summonerName, id);
+    const summonerId = await fetchSummonerId(summonerName!, id);
 
-    const response = await fetchChampionMastery(summonerId)
+    const response = await fetchChampionMastery(summonerId);
+    const body = { championMastery: response, summonerId }
     return {
       statusCode: 200,
-      body: `{ "championMastery": ${response}, ${summonerId} }`
+      body: JSON.stringify(body),
     };
   }
   return {
     statusCode: 400,
-    body: "Error: Include either a summerName or summonerId"
+    body: "Error: Include either a summerName or summonerId",
   };
 };
 
