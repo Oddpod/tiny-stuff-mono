b38588f3f4d662e85ed171be6804162dd329a564
Odd Harald Sjursen Sande <oddh123@gmail.com>
2022-03-19 15:55:10 +0100

    lch: Add pwa support

--- league-chest-hunter/vite.config.ts
+++ league-chest-hunter/vite.config.ts
@@ -1,8 +1,39 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'League Chest Hunter',
+        short_name: 'League Chest Hunter',
+        description: "View available chests for your champions",
+        theme_color: '#115577',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    })
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
