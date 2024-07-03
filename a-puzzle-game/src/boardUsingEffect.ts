f23d72fcf9c4e4d6e06b86b2394819ee9b05eed8
Oddpod <oddh123@gmail.com>
2024-07-03 22:51:39 +0200

    feat: :construction: Rough pipeline for creating a new puzzle

--- a-puzzle-game/src/boardUsingEffect.ts
+++ a-puzzle-game/src/boardUsingEffect.ts
@@ -1,17 +1,24 @@
-import { Effect, pipe } from "effect";
+import { Effect, Logger, LogLevel } from "effect";
+import type { PieceEntity } from "./pieceCreator";
+import { findFittingPiece } from "./piecePicker";
 
 const dimensionsConfig = document.getElementById("select-piece-dimensions") as HTMLFormElement
 
-const inputWidth = dimensionsConfig.querySelector('input[id="dim-width]"') as HTMLInputElement
-const inputHeight = dimensionsConfig.querySelector('input[id="dim-height]"') as HTMLInputElement
+const inputWidth = dimensionsConfig.querySelector('#dim-width') as HTMLInputElement
+const inputHeight = dimensionsConfig.querySelector('#dim-height') as HTMLInputElement
 
 
 dimensionsConfig.addEventListener("submit", (event) => {
     event.preventDefault();
-    createNewBoard();
+    createPuzzle();
 })
 
-const readDimConfig = () => {
+type DimConfig = {
+    width: number,
+    height: number
+}
+
+const readDimConfig = (): Effect.Effect<DimConfig, Error> => {
     const width = Number.parseInt(inputWidth.value)
     const height = Number.parseInt(inputHeight.value)
 
@@ -20,9 +27,67 @@ const readDimConfig = () => {
     }
     return Effect.succeed({
         width,
-        height
+        height,
     })
 }
 
 
-const createNewBoard = () => pipe(readDimConfig)
+type DimConfigWithPieceSize = DimConfig & { pieceSize: number }
+let uniqueCounter = 0
+const createBoard = ({ width, height, pieceSize }: DimConfigWithPieceSize): Effect.Effect<PieceEntity[][], Error> => {
+    const pieces: PieceEntity[][] = [];
+    for (let j = 0; j < height; j++) {
+        const row: PieceEntity[] = [];
+        let toTheLeft = undefined;
+        let toTheTop = undefined;
+        const isLastRow = j === height
+        for (let i = 0; i < width; i++) {
+            toTheTop = pieces[j - 1]?.[i]?.definition.sides.bottom
+            const pieceDef = findFittingPiece({
+                toTheLeft,
+                toTheTop,
+                lastInRow: i === width,
+                isLastRow,
+            });
+            row.push({
+                id: uniqueCounter++,
+                boundingBox: [
+                    {
+                        x: i * pieceSize,
+                        y: j * pieceSize
+                    },
+                    {
+                        x: (i + 1) * pieceSize,
+                        y: (j + 1) * pieceSize
+                    }
+                ],
+                definition: pieceDef
+            })
+
+            toTheLeft = pieceDef.sides.right;
+        }
+        pieces.push(row)
+    }
+    return Effect.succeed(pieces)
+}
+
+const calculateAndAddPieceSize = Effect.flatMap(({ width, height }: DimConfig) => {
+    // TODO: Calculate actual pieceSize
+    return Effect.succeed({ width, height, pieceSize: 50 } as DimConfigWithPieceSize)
+})
+
+const placePieces = (board: PieceEntity[][]) => {
+    // TODO: place pieces
+    return Effect.succeed(1)
+}
+
+export const createPuzzle = () => Effect.runSync(
+    readDimConfig().pipe(
+        Effect.tap((dim) => Effect.logDebug({ dim })),
+        calculateAndAddPieceSize,
+        Effect.tap((withPieceSize) => Effect.logDebug({ withPieceSize })),
+        Effect.flatMap(createBoard),
+        Effect.tap((board) => Effect.logDebug({ board })),
+        Effect.flatMap(placePieces),
+        Logger.withMinimumLogLevel(LogLevel.Debug)
+    ))
--- a-puzzle-game/src/main.ts
+++ a-puzzle-game/src/main.ts
@@ -2,6 +2,7 @@ import "./style.css";
 import { previewFile, resetToDefaultImage } from "./previewFile";
 import { loadSavedState, saveBoardState } from "./storeState";
 import { BoardCreator } from "./board";
+import { createPuzzle } from "./boardUsingEffect";
 
 const boardElement = document.getElementById("board") as HTMLDivElement;
 const boardContainer = document.getElementById("board-container") as HTMLDivElement
@@ -22,66 +23,68 @@ const pieceSizeSelector = document.getElementById(
 	"select-piece-size",
 )! as HTMLSelectElement;
 
-const dimensionsConfig = document.getElementById("select-piece-dimensions") as HTMLFormElement
+// const dimensionsConfig = document.getElementById("select-piece-dimensions") as HTMLFormElement
 
-const inputWidth = dimensionsConfig.querySelector('input[id="dim-width"') as HTMLInputElement
-const inputHeight = dimensionsConfig.querySelector('input[id="dim-height"') as HTMLInputElement
-dimensionsConfig.addEventListener("submit", (event) => {
-	event.preventDefault();
-	const dimWidth = inputWidth.value;
-	const dimHeight = inputHeight.value;
-	// TODO: Create board
-	// TODO: --> place pieces --> 
-})
+// const inputWidth = dimensionsConfig.querySelector('input[id="dim-width"') as HTMLInputElement
+// const inputHeight = dimensionsConfig.querySelector('input[id="dim-height"') as HTMLInputElement
+// dimensionsConfig.addEventListener("submit", (event) => {
+// 	event.preventDefault();
+// 	const dimWidth = inputWidth.value;
+// 	const dimHeight = inputHeight.value;
+// 	// TODO: Create board
+// 	// TODO: --> place pieces --> 
+// })
+
+createPuzzle()
 
 
-const boardCreator = new BoardCreator({
-	boardContainer,
-	boardElement,
-	pieceGap: PIECE_GAP,
-	pieceSize: Number(pieceSizeSelector.value),
-	pieceMovedCallback: ({ pieceId, left, top }) => {
-		boardCreator.piecePositions.set(pieceId, { left, top });
-		saveBoardState({
-			board: boardCreator.board,
-			imageSrc: previewImageElement.src,
-			scaleFactorX: boardCreator.meta.scaleFactorX,
-			scaleFactorY: boardCreator.meta.scaleFactorY,
-			piecePositions: boardCreator.piecePositions,
-		});
-	},
-});
+// const boardCreator = new BoardCreator({
+// 	boardContainer,
+// 	boardElement,
+// 	pieceGap: PIECE_GAP,
+// 	pieceSize: Number(pieceSizeSelector.value),
+// 	pieceMovedCallback: ({ pieceId, left, top }) => {
+// 		boardCreator.piecePositions.set(pieceId, { left, top });
+// 		saveBoardState({
+// 			board: boardCreator.board,
+// 			imageSrc: previewImageElement.src,
+// 			scaleFactorX: boardCreator.meta.scaleFactorX,
+// 			scaleFactorY: boardCreator.meta.scaleFactorY,
+// 			piecePositions: boardCreator.piecePositions,
+// 		});
+// 	},
+// });
 
-pieceSizeSelector.addEventListener("change", (event) => {
-	const target = event.target as HTMLSelectElement;
-	boardCreator.setPieceSize(Number(target.value ?? DEFAULT_PIECE_SIZE));
-});
+// pieceSizeSelector.addEventListener("change", (event) => {
+// 	const target = event.target as HTMLSelectElement;
+// 	boardCreator.setPieceSize(Number(target.value ?? DEFAULT_PIECE_SIZE));
+// });
 
-fileUpload?.addEventListener("change", () => previewFile());
+// fileUpload?.addEventListener("change", () => previewFile());
 
-const loadButton = document.getElementById("load-button") as HTMLButtonElement;
-loadButton?.addEventListener("click", () => {
-	boardCreator.resetPiecePositions();
-	boardCreator.createPuzzle(previewImageElement.src);
-});
-const resetButton = document.getElementById("reset-button")!;
-resetButton.addEventListener("click", () => {
-	try {
-		loadButton.setAttribute("disabled", "");
-		localStorage.clear();
-		resetToDefaultImage();
-		boardCreator.createPuzzle(previewImageElement.src);
-	} finally {
-		setTimeout(() => loadButton.removeAttribute("disabled"), 500);
-	}
-});
-loadSavedState(
-	({ piecePositions, board, ...rest }) => {
-		boardCreator.setPiecePositions(piecePositions);
-		boardCreator.board = board;
-		boardCreator.cutAndPlacePieces(rest);
-	},
-	async () => {
-		await boardCreator.createPuzzle(previewImageElement.src);
-	},
-);
+// const loadButton = document.getElementById("load-button") as HTMLButtonElement;
+// loadButton?.addEventListener("click", () => {
+// 	boardCreator.resetPiecePositions();
+// 	boardCreator.createPuzzle(previewImageElement.src);
+// });
+// const resetButton = document.getElementById("reset-button")!;
+// resetButton.addEventListener("click", () => {
+// 	try {
+// 		loadButton.setAttribute("disabled", "");
+// 		localStorage.clear();
+// 		resetToDefaultImage();
+// 		boardCreator.createPuzzle(previewImageElement.src);
+// 	} finally {
+// 		setTimeout(() => loadButton.removeAttribute("disabled"), 500);
+// 	}
+// });
+// loadSavedState(
+// 	({ piecePositions, board, ...rest }) => {
+// 		boardCreator.setPiecePositions(piecePositions);
+// 		boardCreator.board = board;
+// 		boardCreator.cutAndPlacePieces(rest);
+// 	},
+// 	async () => {
+// 		await boardCreator.createPuzzle(previewImageElement.src);
+// 	},
+// );
